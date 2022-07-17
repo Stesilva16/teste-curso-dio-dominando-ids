da610ab7c3f16e3fe074ece24df385f821f0ffcc
Stesilva16 <stefany.silva2020.16@gmail.com>
2022-07-17 10:37:00 -0300

    Fisrt Commit

---
+++ src/brcom/dio/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package brcom.dio;
+
+import brcom.dio.model.gato;
+
+public class PrimeiroPrograma {
+	
+
+	public static void main (String []arg) {
+		
+		gato Gato = new gato ();
+		
+		System.out.println (Gato);	
+		
+		/*int a =2;
+		int b =3;
+		System.out.print("Hello World " + (a+b));*/
+	}
+		
+	
+}
